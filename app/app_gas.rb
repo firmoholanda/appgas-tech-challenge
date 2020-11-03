e2f866e6262f0bb344711938942f0355b96858a2
firmo holanda <firmo@viize.com>
2020-11-03 12:45:02 -0300

    Update app_gas.rb

--- app/app_gas.rb
+++ app/app_gas.rb
@@ -1,6 +1,13 @@
+require_relative "./controller/appgas.rb"
+require_relative "./controller/item.rb"
+require_relative "./controller/product.rb"
+require_relative "./controller/shipment.rb"
+
+# ------------------------------------------------------------------------------------- #
+
 my_app = AppGas.new
 
-my_app.load_css("app_gas.csv")
-#my_app.print_products
-#my_app.print_shipments
+my_app.load_css("../data/app_gas.csv")
+my_app.print_products
+my_app.print_shipments
 my_app.print_json
